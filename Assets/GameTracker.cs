bbc2d352d461d37256f3422f629e966406627dab
juliusvanleeuwen <58423129+juliusvanleeuwen@users.noreply.github.com>
2022-06-14 10:10:35 +0200

    Fix 2d loading

---
+++ Assets/GameTracker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTracker : MonoBehaviour
+{
+    public static bool OrderPicked = false;
+    public static bool TruckLoaded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
